79f26e475ec4028abb01fe2f0c95fad034332a34
angela <angela@aviak.ca>
2021-06-24 20:28:42 -0600

    declaring and using variables and arithmetic operators

---
+++ src/ExampleVariable.java
@@ -0,0 +1,24 @@
+/*
+This demonstrates a variable
+ */
+class ExampleVariable {
+    public static void main(String args[]) {
+        // operators
+        // + - * /
+
+        int myVar1; // this declares a variable
+        int myVar2; // this declares another variable
+
+        myVar1 = 1024; //this assigns 1024 to myVar1
+        System.out.println("myVar1 contains " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.print("myVar2 contains myVar1 / 2: ");
+        System.out.println(myVar2);
+        System.out.println(); //print a blank line
+        // types
+        // int, float, double
+
+    }
+}
