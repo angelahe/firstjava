6bd36a0a9f2377e2a26d3e73bc663c383ea2fd35
angela <angela@aviak.ca>
2021-06-25 05:21:11 -0600

    if, for statements in java

---
+++ src/ExampleControls.java
@@ -0,0 +1,33 @@
+class ExampleControls {
+    public static void main(String args[]) {
+        if (10 < 11) System.out.println("10 is less than 11");
+
+        // operators
+        // < <= > >= == !=
+
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if(a < b) System.out.println("a is less than b");
+
+        c = a - b; //c contains -1
+
+        System.out.println("c contains -1");
+        if(c < 0) System.out.println("c is negative");
+
+        int count;
+
+        for(count = 0; count < 5; count++)
+            System.out.println("This is count: " + count);
+
+        // blocks of code
+
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+    }
+}
